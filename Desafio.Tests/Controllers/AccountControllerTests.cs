6c5220dcedb6f5ccf63b938815c1e6d1206cddc0
BrunoHiramuki <hiramuki15@gmail.com>
2025-10-07 00:27:27 -0300

    test: add unit test project

---
+++ Desafio.Tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Desafio.Controllers;
+using Desafio.Models;
+using Desafio.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Desafio.Tests.Controllers
+{
+    public class AccountControllerTests
+    {
+        private readonly Mock<IAccountService> _mockService;
+        private readonly AccountController _controller;
+
+        public AccountControllerTests()
+        {
+            _mockService = new Mock<IAccountService>();
+            _controller = new AccountController(_mockService.Object);
+        }
+
+        [Fact]
+        public void Read_ShouldReturnAllAccounts()
+        {
+            var accounts = new List<Account>
+            {
+                new Account {
+                    Name = "Teste1",
+                    Value = 100,
+                    DueDate = new DateTime(2024, 01, 10),
+                    PaymentDate = new DateTime(2024, 01, 05)
+                },
+                new Account {
+                    Name = "Teste2",
+                    Value = 100,
+                    DueDate = new DateTime(2024, 01, 10),
+                    PaymentDate = new DateTime(2024, 01, 10) 
+                }
+            };
+
+            _mockService.Setup(service => service.GetAccount())
+                       .Returns(accounts);
+
+            var result = _controller.Read();
+
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnedAccounts = Assert.IsType<List<Account>>(okResult.Value);
+            Assert.Equal(2, returnedAccounts.Count);
+        }
+
+        [Fact]
+        public void Create_ShouldCallService()
+        {
+            var account = new Account {
+                Name = "Teste3",
+                Value = 100,
+                DueDate = new DateTime(2024, 01, 10),
+                PaymentDate = new DateTime(2024, 01, 05)
+            };
+
+            var result = _controller.Create(account);
+
+            _mockService.Verify(service => service.CreateAccount(account), Times.Once);
+            Assert.IsType<OkResult>(result);
+        }
+    }
+}
---
+++ Desafio.Tests/Repositories/ccountRepositoryTests.cs
@@ -0,0 +1,112 @@
+﻿using Desafio.Context;
+using Desafio.Models;
+using Desafio.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Desafio.Tests.Repositories
+{
+    public class AccountRepositoryTests : IDisposable
+    {
+        private readonly AppDbContext _context;
+        private readonly AccountRepository _repository;
+
+        public AccountRepositoryTests()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new AppDbContext(options);
+            _repository = new AccountRepository(_context);
+
+            SeedDatabase();
+        }
+
+        private void SeedDatabase()
+        {
+            _context.Penalties.AddRange(
+                new Penaltie { DaysLateFrom = 1, DaysLateTo = 4, FinePercent = 2, DailyInterest = 0.1m },
+                new Penaltie { DaysLateFrom = 4, DaysLateTo = 6, FinePercent = 3, DailyInterest = 0.2m },
+                new Penaltie { DaysLateFrom = 6, DaysLateTo = null, FinePercent = 5, DailyInterest = 0.3m }
+            );
+            _context.SaveChanges();
+        }
+
+        [Fact]
+        public void GetPenalties_3DaysLate_ShouldReturnFirstRule()
+        {
+            var result = _repository.GetPenalties(3);
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.FinePercent);
+            Assert.Equal(0.1m, result.DailyInterest);
+        }
+
+        [Fact]
+        public void GetPenalties_5DaysLate_ShouldReturnSecondRule()
+        {
+            var result = _repository.GetPenalties(5);
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.FinePercent);
+            Assert.Equal(0.2m, result.DailyInterest);
+        }
+
+        [Fact]
+        public void GetPenalties_10DaysLate_ShouldReturnThirdRule()
+        {
+            var result = _repository.GetPenalties(10);
+
+            Assert.NotNull(result);
+            Assert.Equal(5, result.FinePercent);
+            Assert.Equal(0.3m, result.DailyInterest);
+        }
+
+        [Fact]
+        public void GetAllAccount_ShouldReturnAllAccounts()
+        {
+            _context.Accounts.Add(new Account {
+                Name = "Teste1",
+                Value = 100,
+                DueDate = new DateTime(2024, 01, 10),
+                PaymentDate = new DateTime(2024, 01, 05)
+            });
+            _context.Accounts.Add(new Account
+            {
+                Name = "Teste2",
+                Value = 200,
+                DueDate = new DateTime(2024, 01, 10),
+                PaymentDate = new DateTime(2024, 01, 10)
+            });
+            _context.SaveChanges();
+
+            var result = _repository.GetAllAccount();
+
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public void AddAccount_ShouldIncreaseCount()
+        {
+            var initialCount = _context.Accounts.Count();
+            var account = new Account {
+                Name = "Teste3",
+                Value = 300,
+                DueDate = new DateTime(2024, 01, 10),
+                PaymentDate = new DateTime(2024, 01, 05)
+            };
+
+            _repository.AddAccount(account);
+
+            Assert.Equal(initialCount + 1, _context.Accounts.Count());
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
